6888c5ec3e02b43b8c22721d33ec9832a66f7fde
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2025-01-09 19:39:01 +0100

    connect to server to test connection string

---
+++ DumplingsEFCore/DumplingsEFCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using publisherData;
+namespace DumplingsEFCore
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            using (PubContext context = new PubContext())
+            {
+                context.Database.EnsureCreated();
+            }
+
+        }
+    }
+}
---
+++ DumplingsEFCore/publisherData/PubContext.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace publisherData
+{
+    public class PubContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        /// <summary>
+        /// Configures the database connection, bra för att lära sigg men ska inte hardkodas i vanliga projekt, använd config fil
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=DESKTOP-V7OGRKD;Database=TestDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+
+    }
+
+
+    public class Author
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<Book> Books
+        { get; set; } = new List<Book>();
+    }
+
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public DateOnly PublishDate { get; set; }
+        public decimal BasePrice { get; set; }
+        public Author Author { get; set; }
+        public int AuthorId { get; set; }
+    }
+
+
+}
