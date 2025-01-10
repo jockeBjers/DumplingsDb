f6ce812bd86c4be425d789ae040ed755e294084b
Joakim Bjerselius <176925453+jockeBjers@users.noreply.github.com>
2025-01-10 15:14:10 +0100

    add the logic for close and minimize button

--- DumplingsEFCore/DumplingWPF/MainWindow.xaml.cs
+++ DumplingsEFCore/DumplingWPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Security.Policy;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,7 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
 using publisherData;
+using MenuItem = publisherData.MenuItem;
 
 
 namespace DumplingWPF
@@ -18,12 +22,36 @@ namespace DumplingWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly PubContext context;
+        public FoodItemsViewModel FoodViewModel { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            context = new PubContext();
+            FoodViewModel = new FoodItemsViewModel(context);
+            DataContext = FoodViewModel;
+
+            DataContext = this;
         }
 
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();  // Close
+        }
 
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
 
+                this.DragMove(); // drag window
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; // Minimizes
+        }
     }
 }
