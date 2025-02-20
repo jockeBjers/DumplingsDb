97e706689c0c85d7121701be861872320c1d685a
JockeBjers <joakim.bjerselius@hotmail.com>
2025-02-20 13:22:45 +0100

    order endpoint to search by id

--- DumplingProjects/DumplingApi/Endpoints/OrderEndPoints.cs
+++ DumplingProjects/DumplingApi/Endpoints/OrderEndPoints.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
 using publisherData;
 
 namespace DumplingApi.Endpoints;
@@ -8,7 +9,7 @@ public class OrderEndPoints
 
     public static void Map(WebApplication app)
     {
-
+        // Get orders
         app.MapGet("/api/orders", async (PubContext dbContext) =>
         {
             return await dbContext.Orders
@@ -21,14 +22,47 @@ public class OrderEndPoints
                     Customer = new { o.CustomerId, o.Customer!.Name },
                     Items = o.Items.Select(i => new
                     {
-                        i.MenuItem,
+                        i.MenuItem.Name,
+                        i.MenuItem.Price,
                         i.Quantity
                     })
                 })
                 .ToListAsync();
         });
 
+        // get orders by id
+        app.MapGet("/app/orders/{id}", async (int id, PubContext dbContext) =>
+        {
 
+            var order = await dbContext.Orders
+                .Where(o => o.Id == id)
+                .Include(o => o.Customer)
+                .Include(o => o.Items)
+                .ThenInclude(i => i.MenuItem)
+                .FirstOrDefaultAsync();
+
+            if (order == null) return Results.NotFound();
+
+            return Results.Ok(new
+            {
+                order.Id,
+                order.OrderDate,
+                order.TotalPrice,
+                order.IsCompleted,
+                Customer = new
+                {
+                    order.Customer!.Id,
+                    order.Customer.Name,
+                    order.Customer.Telephone,
+                },
+                Items = order.Items.Select(i => new
+                {
+                    i.MenuItem,
+                    i.Quantity,
+                })
+
+            });
+        });
 
 
 
