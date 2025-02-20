8a739410b5bb93b835e94e1ada5d78997fa4e357
JockeBjers <joakim.bjerselius@hotmail.com>
2025-02-20 09:38:01 +0100

    add order endpoint with .Select to filter out cross reference in customer

---
+++ DumplingProjects/DumplingApi/Endpoints/OrderEndPoints.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using publisherData;
+
+namespace DumplingApi.Endpoints;
+
+public class OrderEndPoints
+{
+
+    public static void Map(WebApplication app)
+    {
+
+        app.MapGet("/api/orders", async (PubContext dbContext) =>
+        {
+            return await dbContext.Orders
+                .Select(o => new
+                {
+                    o.Id,
+                    o.OrderDate,
+                    o.TotalPrice,
+                    o.IsCompleted,
+                    Customer = new { o.CustomerId, o.Customer!.Name },
+                    Items = o.Items.Select(i => new
+                    {
+                        i.MenuItem,
+                        i.Quantity
+                    })
+                })
+                .ToListAsync();
+        });
+
+
+
+
+
+
+    }
+}
