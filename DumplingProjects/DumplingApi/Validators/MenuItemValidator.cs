1f273480f4f928eb826d10e7f0515f71252dcc4d
JockeBjers <joakim.bjerselius@hotmail.com>
2025-03-07 09:18:24 +0100

    add validation

---
+++ DumplingProjects/DumplingApi/Validators/MenuItemValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using publisherData;
+
+namespace DumplingApi.Validators;
+
+
+public class MenuItemValidator : AbstractValidator<MenuItem>
+{
+    public MenuItemValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("The item needs a name");
+        RuleFor(x => x.Category).NotEmpty().WithMessage("The item needs a category");
+        RuleFor(x => x.Description).NotEmpty().WithMessage("The item needs a description");
+        RuleFor(x => x.Price).NotEmpty().WithMessage("The item needs a price")
+            .GreaterThan(0).WithMessage("The price must be greater than 0");
+    }
+}
---
+++ DumplingProjects/DumplingApi/Validators/OrderValidator.cs
@@ -0,0 +1,29 @@
+﻿using DumplingApi.Services;
+using FluentValidation;
+
+namespace DumplingApi.Validators;
+public class OrderDtoValidator : AbstractValidator<OrderDto>
+{
+    public OrderDtoValidator()
+    {
+        RuleFor(o => o.OrderDate)
+            .NotEmpty().WithMessage("Order date is required.")
+            .LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Order date cannot be in the future.");
+
+        RuleFor(o => o.Items)
+            .NotEmpty().WithMessage("An order must have at least one item.")
+            .ForEach(item => item.SetValidator(new OrderItemDtoValidator()));
+    }
+}
+
+public class OrderItemDtoValidator : AbstractValidator<OrderItemDto>
+{
+    public OrderItemDtoValidator()
+    {
+        RuleFor(oi => oi.MenuItemName)
+            .NotEmpty().WithMessage("Menu item name is required.");
+
+        RuleFor(oi => oi.Quantity)
+            .GreaterThan(0).WithMessage("Quantity must be greater than zero.");
+    }
+}
